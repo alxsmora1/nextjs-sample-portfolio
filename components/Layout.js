79e61e31b1212a79ae6d7f0909ba11553a5dcc52
alxsmora1 <alexis.mora1v@gmail.com>
2021-03-18 02:14:39 -0600

    Create a Layout component
    Create the Layout component to contain the pages of the project passing
    the components with destructuring of the children component props.
    Add the component Navbar in the top of the layout.
    Add the container div to layout.

---
+++ components/Layout.js
@@ -0,0 +1,12 @@
+import Navbar from "./Navbar";
+
+const Layout = ({children}) => (
+    <>
+        <Navbar />
+        <div className="container">
+            {children}
+        </div>
+    </>
+);
+
+export default Layout;
