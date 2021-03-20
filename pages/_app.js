8ed28e26820495743bfbed50722103b53fcfab99
alxsmora1 <alexis.mora1v@gmail.com>
2021-03-19 19:55:04 -0600

    Fix the path to favicon and the propierty
    Change the favicon path to public/.
    Fix the propierty charset to charSet.

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
+import Head from "next/head";
 import 'bootstrap/dist/css/bootstrap.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>  
+    <Head>
+        <title>React Sample Portfolio</title>
+        <meta charSet="utf-8" /> 
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="favicon.ico" /> 
+    </Head>
+
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
